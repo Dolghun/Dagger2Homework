35ee0b74f07c9ee25597dade586877d4f300254b
Dolghun Mustafayev <d.mustafaev@sigma-it.ru>
2022-03-25 16:31:49 +0300

    Dagger

--- app/src/main/java/ru/otus/daggerhomework/MainActivity.kt
+++ app/src/main/java/ru/otus/daggerhomework/MainActivity.kt
@@ -3,9 +3,29 @@ package ru.otus.daggerhomework
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DependenciesProvider<MainActivityComponent> {
+    
+    lateinit var activityComponent: MainActivityComponent
+    
     override fun onCreate(savedInstanceState: Bundle?) {
+        val dependenciesProvider = application as? DependenciesProvider<*>
+            ?: throw ClassCastException(
+                "App must implement `DependenciesProvider` of `MainActivityDependencies`"
+            )
+        activityComponent = DaggerMainActivityComponent.factory()
+            .create(dependenciesProvider.getDependencies() as MainActivityDependencies, this)
+        
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fReceiver, FragmentReceiver())
+                .replace(R.id.fProducer, FragmentProducer())
+                .commit()
+        }
     }
+    
+    override fun getDependencies(): MainActivityComponent = activityComponent
+    
 }
